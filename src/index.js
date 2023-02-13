ec734fc5f6da100762251a4b72a22e0b2fb6d9cb
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-02-13 11:11:10 +0100

    setting webpack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 // якщо хочему щоб зміну у бразері були автоматичні розкомментувати нижче
-// import './index.html';
-import './index.css';
-
-
-const mult = (a, b) => a * b;
+import './index.html';
+import './index.scss';
+import { mult, sum } from './modules/example1';
 
 console.log(mult(2, 4));
-console.log(mult(3, 5));
+console.log(sum(3, 5));
